9c5e88d3e3ec58c22b5ee6b133bdc122f5eb4cbf
Marco Minerva <marco.minerva@gmail.com>
2022-08-22 11:46:45 +0200

    Add Swagger integration

--- ValidationSample/Program.cs
+++ ValidationSample/Program.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using ValidationSample.BusinessLayer.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,11 @@ builder.Services.AddFluentValidationAutoValidation(options =>
 });
 
 builder.Services.AddValidatorsFromAssemblyContaining<PersonValidator>();
+
+builder.Services.AddFluentValidationRulesToSwagger(options =>
+{
+    options.SetNotNullableIfMinLengthGreaterThenZero = true;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
